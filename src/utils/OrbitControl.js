a3b0257c58f0cbf97dda44c554a0cb1749abcd55
DUSSOUTREVEL Solal <sdussoutrevel@g-a101-24.local>
2018-04-18 12:05:52 +0200

    Fixe bug routes & OrbitControl export

--- src/utils/OrbitControl.js
+++ src/utils/OrbitControl.js
@@ -15,7 +15,7 @@ import * as THREE from "three"
 //    Zoom - middle mouse, or mousewheel / touch: two finger spread or squish
 //    Pan - right mouse, or arrow keys / touch: three finger swipe
 
-THREE.OrbitControls = function ( object, domElement ) {
+var OrbitControls = function ( object, domElement ) {
 
 	this.object = object;
 	this.velocity = new THREE.Vector2();
@@ -933,10 +933,10 @@ THREE.OrbitControls = function ( object, domElement ) {
 
 };
 
-THREE.OrbitControls.prototype = Object.create( THREE.EventDispatcher.prototype );
-THREE.OrbitControls.prototype.constructor = THREE.OrbitControls;
+OrbitControls.prototype = Object.create( THREE.EventDispatcher.prototype );
+OrbitControls.prototype.constructor = OrbitControls;
 
-Object.defineProperties( THREE.OrbitControls.prototype, {
+Object.defineProperties( OrbitControls.prototype, {
 
 	center: {
 
@@ -1061,4 +1061,4 @@ Object.defineProperties( THREE.OrbitControls.prototype, {
 
 } );
 
-export default THREE.OrbitControls; 
+export default OrbitControls; 
